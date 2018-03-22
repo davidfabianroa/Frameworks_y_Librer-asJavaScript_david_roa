052a39e031432be10930d04392868fa66b8ac9be
david fabian roa <davidfabianroa@gmail.com>
2018-03-22 07:36:29 -0500

    parapadeo del titulo

--- nextUcodBaseJuego/js/app.js
+++ nextUcodBaseJuego/js/app.js
@@ -1,9 +1,17 @@
+// JavaScript Document
 // inicia el juego
+$(function() {iniciar();});
 function iniciar() {parpadear('h1.main-titulo');
 	$('.btn-reinicio').click(function () {if ($(this).text() === 'Reiniciar') {location.reload(true);}
 		caramelos_de_pantalla();$(this).text('Reiniciar');$('#timer').startTimer({onComplete: finalizar	})});
 }
+//funcion que permite destellos de color en el titulo
+function parpadear(selector) {
+	$(selector).animate({opacity: '1',}, {step: function () {$(this).css('color', 'white');},queue: true})
+		.animate({opacity: '1'}, {step: function () {$(this).css('color', 'yellow');parpadear('h1.main-titulo');},queue: true})
+		.animate({opacity: '1'}, {step: function () {$(this).css('color', 'white');	},queue: true})
+		.animate({opacity: '1'}, {step: function () {$(this).css('color', 'yellow');},queue: true}, 600).delay(1000);}
 
 
-$(function() {iniciar();});
+
 
